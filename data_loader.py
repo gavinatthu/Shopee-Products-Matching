7a600eb281fc3ddaa97c561a5b2dd79bb2e5b83d
gavinatthu <39040979+gavinatthu@users.noreply.github.com>
2021-04-13 16:13:11 +0800

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -4,13 +4,18 @@ import torchvision
 import torchvision.transforms as transforms
 from PIL import Image
 
+def read_sort(DATA_PATH):
+    train = pd.read_csv(DATA_PATH + 'train.csv')
+    train['image'] = DATA_PATH + 'train_images/' + train['image']
+    tmp = train.groupby('label_group').posting_id.agg('unique').to_dict()
+    train['target'] = train.label_group.map(tmp)
+    train = train.sort_values(by='label_group')
+    return train
+
 
 class Dataloader():
-    def __init__(self, DATA_PATH, height, width):
-        train = pd.read_csv(DATA_PATH + 'train.csv')
-        train['image'] = DATA_PATH + 'train_images/' + train['image']
-        self.img_path = train['image'].values[:100]
-        print(self.img_path)
+    def __init__(self, PATH, height, width):
+        self.img_path = PATH
         self.transform = transforms.Compose([
             transforms.Resize((height, width)),
             transforms.ToTensor(),
