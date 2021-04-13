49fa68f212f0f6aab59aed4bd8fd98087453a842
gavinatthu <39040979+gavinatthu@users.noreply.github.com>
2021-04-13 16:13:53 +0800

    Create Methods.py

---
+++ Methods.py
@@ -0,0 +1,48 @@
+import torch
+import torchvision.models as models
+import torch.nn as nn
+import torch.nn.functional as F
+
+# Convlution Network
+class Conv(nn.Module):
+    def __init__(self):
+        super(Conv, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.bn1 = nn.BatchNorm2d(10)
+        self.bn2 = nn.BatchNorm2d(20)
+        self.mp = nn.MaxPool2d(2)
+        self.fc = nn.Linear(320, 10)
+
+    def forward(self, x):
+        # in_size = 64
+        in_size = x.size(0) # one batch
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.mp(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.mp(x)
+        x = F.relu(x)
+        # x: 64*320
+        x = x.view(in_size, -1) # flatten the tensor
+        # x: 64*10
+        x = self.fc(x)
+        return x
+
+
+# pretrain models
+class ShopeeImageEmbeddingNet(nn.Module):
+    def __init__(self):
+        super(ShopeeImageEmbeddingNet, self).__init__()
+              
+        model = models.resnet50(True)
+        model.avgpool = nn.AdaptiveMaxPool2d(output_size=(1, 1))
+        model = nn.Sequential(*list(model.children())[:-1])
+        model.eval()
+        self.model = model
+        
+    def forward(self, img):        
+        out = self.model(img)
+        return out
