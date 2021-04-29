bfe6a4ad579f77d51ed5b07405d3e3a7434869f2
gavinatthu <39040979+gavinatthu@users.noreply.github.com>
2021-04-29 18:15:43 +0800

    添加textloader

--- data_loader.py
+++ data_loader.py
@@ -3,6 +3,7 @@ from pandas import Series,DataFrame
 import torch
 import torchvision
 import torchvision.transforms as transforms
+import torchtext
 from PIL import Image
 
 def read_sort(DATA_PATH):
@@ -14,18 +15,49 @@ def read_sort(DATA_PATH):
     return train
 # 这里参考https://www.kaggle.com/finlay/shopee-products-matching-image-part-english 需要改写
 
+
+# 改写后的read函数
 def read(DATA_PATH):
     origin = pd.read_csv(DATA_PATH + 'train.csv')    
     origin = origin.sort_values(by='label_group')
-    print(len(origin['label_group'].value_counts()))  # 总共有11014个类
+    #print('Items count:', len(origin))
+    #print('Classes count:', len(origin['label_group'].value_counts()))  # 总共有11014个类
     train = origin[:30013]   #为了使最后一类完整留在train中向后移位
     test = origin[30013:]
     train_path = DATA_PATH + 'train_images/' + train['image'].values
     test_path = DATA_PATH + 'train_images/' + test['image'].values   #这种切分方法实际上并不优雅，考虑用groupby('label_group')，之后random选取一定比例的组
     # 需要写一个交叉验证分割方法，保证每一类至少有一个点在train中，初步验证先用所有点训练
     # 可以先在每个组中随机抽一个点，剩下的点再随机分割
-    
+
     return train, test, train_path, test_path
+
+
+def textLoader(DATA_PATH, BATCH_SIZE):
+    TEXT = torchtext.legacy.data.Field()
+    LABEL = torchtext.legacy.data.Field(sequential=False,dtype=torch.long)
+    fields = [(None, None), (None, None), (None, None), ('title', TEXT), ('label', LABEL)]
+
+    # 这里用torchtext读取csv文件，同样的功能也可以用pandas实现
+    train = torchtext.legacy.data.TabularDataset(
+        path=DATA_PATH + 'train.csv', format='csv',
+        skip_header=True, fields=fields)
+
+    # build the vocabulary, glove6B pretrained model lies in DATA_PATH
+    TEXT.build_vocab(train, vectors=torchtext.vocab.Vectors(name='glove.6B.300d.txt', cache=DATA_PATH))
+
+    # build the Batch Iterator
+    train_iter = torchtext.legacy.data.BucketIterator(train, batch_size=BATCH_SIZE, sort_key=lambda x:len(x.title), sort_within_batch=True,
+                                shuffle=True, repeat=False)
+
+    print('Label of 0#', train_iter.dataset.examples[0].label)
+    print('Title of 0#', train_iter.dataset.examples[0].title)
+    print(TEXT.vocab.stoi[train_iter.dataset.examples[0].title[0]])
+    #print('Title of 0#', TEXT.vocab.vectors[TEXT.vocab.stoi['you']])
+    return TEXT, train_iter
+
+
+
+
 
 class Dataloader():
     def __init__(self, PATH, height, width):
@@ -43,8 +75,9 @@ class Dataloader():
     def __len__(self):
         return len(self.img_path)
 
+'''
 # 改为相对路径，数据在上层文件夹的shopee-product-matching中
-DATA_PATH = '..\\shopee-product-matching\\'
+DATA_PATH = '/data/shopee_product_matching/'
 BATCH_SIZE = 100
 IMG_SIZE = 512
 
@@ -55,4 +88,5 @@ imageloader = torch.utils.data.DataLoader(imagedataset, BATCH_SIZE, shuffle=Fals
 
 
 #print(test.head())
-#print(train_path)
+print(train_path)
+'''
