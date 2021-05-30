443b0fa2312becd715bfbd0d097ed0aebc7ead8f
gavinatthu <39040979+gavinatthu@users.noreply.github.com>
2021-05-30 23:17:26 +0800

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -1,9 +1,8 @@
 """Evaluate the prediction of model given results. 
-
-TODO: implement AUC, mAP"""
-
+"""
 import numpy as np
-from sklearn.metrics import f1_score
+from sklearn.metrics import f1_score, roc_auc_score, average_precision_score
+
 
 def calAccuracy(y_true, y_pred, topN=1):
     """Calculate Accuracy. 
@@ -41,7 +40,7 @@ def conv2Top1(y_true, y_pred, topN=1):
     """
     y_pred_top1 = np.array(y_pred[:,0]) # copy 1st column of y_pred
     for i, pred in enumerate(y_pred):
-        if np.isin(y_true[i], pred[:topN]):
+        if np.isin(y_true[i], pred[:topN]).any():
             y_pred_top1[i] = y_true[i]
     return y_pred_top1
     
@@ -64,16 +63,80 @@ def calF1score(y_true, y_pred, topN=1, average='micro'):
     """
     return f1_score(y_true, conv2Top1(y_true, y_pred, topN), average=average)
 
+
+
+def modelEvaluation(testFeat, testLabel, trainFeat, trainLabel, *method):
+    """Evaluate the model by calculatint F1-score, AUC or mAP corresponding to topN accuracy. 
+
+    Evaluate the model by calculatint F1-score, AUC and mAP corresponding to topN accuracy. 
+    Top-N predictions are given by the max-N dot products of test set features and training set features. 
+
+    Args:
+        testFeat: test set features predicted by model, # of test samples by # of features array
+        testLabel: test set labels (ground truths), # of test samples array
+        trainFeat: training set features predicted by model, # of training samples by # of features array
+        trainLabel: training set labels (ground truths), # of training samples array
+        method: {'F1', 'AUC', 'mAP'}, if use F1, topN should be provided. if not, topN == 1. Example: modelEvaluation(testFeat, testLabel, trainFeat, trainLabel, 'F1', 2)
+
+    Returns:
+        eval: evaluation result corresponding to the supposed criteria
+    """
+    cor = np.dot(testFeat, trainFeat.T)
+    y_true = testLabel
+    if method[0] == 'F1':
+        if len(method) == 1:
+            topN = 1
+        else:
+            topN = method[1]
+        top_index = np.argsort(-cor, axis=1)[:,:topN]
+        y_pred = (np.asarray(trainLabel).T)[top_index]
+        eval = f1_score(y_true, conv2Top1(y_true, y_pred, topN), average='micro')
+    elif method[0] == 'AUC':
+        y_true_multiLabel = np.zeros((len(testLabel),int(max(max(trainLabel), max(testLabel)))+1))
+        for i, testSample in enumerate(y_true):
+            y_true_multiLabel[i,testSample] = 1
+        predict_proba = np.array([row/sum(row) for row in cor])
+        y_pred = np.zeros((len(testLabel),int(max(max(trainLabel), max(testLabel)))+1))
+        for i, col in enumerate(predict_proba.T):
+            y_pred[:,trainLabel[i]] += col
+        eval = roc_auc_score(y_true, y_pred, multi_class='ovo', average='macro', labels=[i for i in range(int(max(max(trainLabel), max(testLabel)))+1)])
+    elif method[0] == 'mAP':
+        y_true_multiLabel = np.zeros((len(testLabel),int(max(max(trainLabel), max(testLabel)))+1))
+        for i, testSample in enumerate(y_true):
+            y_true_multiLabel[i,testSample] = 1
+        predict_proba = np.array(cor)
+        y_pred = np.zeros((len(testLabel),int(max(max(trainLabel), max(testLabel)))+1))
+        for i, col in enumerate(predict_proba.T):
+            y_pred[:,trainLabel[i]] += col
+        eval = average_precision_score(y_true_multiLabel, y_pred, average='micro')
+    return eval
+
+
+
 # test case
 if __name__ == '__main__':
-    y_true = np.asarray([1, 1, 2, 3])
-    y_pred = np.asarray([[1, 2],
-                        [2, 1], 
-                        [1, 3], 
-                        [2, 3]])
-    print(conv2Top1(y_true, y_pred, topN=2))
-    print(calAccuracy(y_true, y_pred, topN=2))
-    print(calF1score(y_true, y_pred, topN=2))
-    print(conv2Top1(y_true, y_pred, topN=1))
-    print(calAccuracy(y_true, y_pred, topN=1))
-    print(calF1score(y_true, y_pred, topN=1))
+    # y_true = np.asarray([1, 1, 2, 3])
+    # y_pred = np.asarray([[1, 2],
+    #                     [2, 1], 
+    #                     [1, 3], 
+    #                     [2, 3]])
+    # print(conv2Top1(y_true, y_pred, topN=2))
+    # print(calAccuracy(y_true, y_pred, topN=2))
+    # print(calF1score(y_true, y_pred, topN=2))
+    # print(conv2Top1(y_true, y_pred, topN=1))
+    # print(calAccuracy(y_true, y_pred, topN=1))
+    # print(calF1score(y_true, y_pred, topN=1))
+    testFeat = np.asarray([[1, 0, 0],
+                            [0, 1, 0],
+                            [0, 0, 1],
+                            [0.9, 0.1, 0]])
+    # testLabel = np.asarray([0, 1, 2, 1])
+    testLabel = [2, 3, 4, 9]
+    trainFeat = np.asarray([[1, 0, 0],
+                            [0, 1, 0],
+                            [0, 0, 1],
+                            [0, 1, 1],
+                            [1, 0.2, 0.2]])
+    # trainLabel = np.asarray([0, 1, 2, 2, 1])
+    trainLabel = [2, 3, 4, 5, 3]
+    print(modelEvaluation(testFeat, testLabel, trainFeat, trainLabel, 'mAP'))
